eed823cc3fe5649469a0c61ec6e22852cc77c635
prashanth17 <prashant.muthoz@gmail.com>
2023-04-23 09:21:56 +0530

    added comments

--- src/main/java/com/lazy/springboot/Application.java
+++ src/main/java/com/lazy/springboot/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+		//main class runs from here
         SpringApplication.run(Application.class, args);
     }
 
