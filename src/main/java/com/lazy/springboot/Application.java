1e9ff8b4a6a5ec16e30f9e54dba64ad0326a0604
prashanth17 <prashant.muthoz@gmail.com>
2023-04-24 11:05:54 +0530

    HIS-300 realted changes

--- src/main/java/com/lazy/springboot/Application.java
+++ src/main/java/com/lazy/springboot/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+private static final String REST_URL = "www.google.com";
 
     public static void main(String[] args) {
 		//main class runs from here
