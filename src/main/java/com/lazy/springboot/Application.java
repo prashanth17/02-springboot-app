a0d708ceddbe329baa20c15afdd2a614666938c8
prashanth17 <prashant.muthoz@gmail.com>
2023-04-24 11:16:02 +0530

    HIS-301 realted changes

--- src/main/java/com/lazy/springboot/Application.java
+++ src/main/java/com/lazy/springboot/Application.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+private Map<String,Object> cache = new HashMap<String,Object>(); 
     public static void main(String[] args) {
 		//main class runs from here
         SpringApplication.run(Application.class, args);
     }
 
+
+public void loadDataToCache(){
+	//load the data add to the map
+}
 }
